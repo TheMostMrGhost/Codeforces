7e7c31a0da6b6723f3f3041c21672db32795e352
ghost <mid768724@gmail.com>
2023-09-13 20:46:37 +0200

    two sum done

---
+++ two_sum.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+            for (ii, &elem1) in nums.iter().enumerate() {
+        for (jj, &elem2) in nums.iter().enumerate().skip(ii + 1) {
+            if elem1 + elem2 == target {
+                return vec![ii as i32, jj as i32];
+            }
+        }
+    }
+    return vec![0,0];
+
+    }
+}
